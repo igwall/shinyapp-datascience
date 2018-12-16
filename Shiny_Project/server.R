45f6a0f64e7487857d1ff39946767d4a4baebe45
Lucas Gonçalves <lucas.pro@mail.com>
2018-12-16 23:51:14 +0100

    Add presenceTiers et representationJeunes

--- Shiny_Project/server.R
+++ Shiny_Project/server.R
@@ -105,4 +105,150 @@ shinyServer(function(input, output) {
     }
   })
   
+  output$presenceTiersPlot <- renderPlot({
+    #On récupère les personnes ayant eu des accidents
+    accidentés <- subset(data, data$ACCIDENT == 1)
+    
+    # Pour savoir si un accident est aggravé on s'interesse au questionnaire approfondi. 
+    # Pour savoir de quel accident il parle, on doit récupérer l'id de l'accident afin de le relier
+    # à l'accident en question.
+    
+    # Acc1: id 1 -> 5 , Acc2: id 6 -> 10, etc...
+    accidentésAccidentTiers <- select(accidentés, "CHOIXACC", "AUTREUSAGER")
+    str(accidentésAccidentTiers$AUTREUSAGER)
+    
+    
+    # On enlève les accidents avec des données vides:
+    accidentésTiers <- subset(accidentésAccidentTiers, !is.na(AUTREUSAGER))
+    str(accidentésTiers$AUTREUSAGER)
+    
+    # On tris les id par type d'accident:
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(1:5), "ACC"]   <- 1
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(6:10), "ACC"]  <- 2
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(11:15), "ACC"] <- 3
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(16:20), "ACC"] <- 4
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(21:25), "ACC"] <- 5
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(26:30), "ACC"] <- 6
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(31:35), "ACC"] <- 7
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(36:40), "ACC"] <- 8
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(41:45), "ACC"] <- 9
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(46:50), "ACC"] <- 10
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(51:55), "ACC"] <- 11
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(56:60), "ACC"] <- 12
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(61:65), "ACC"] <- 13
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(66:70), "ACC"] <- 14
+    accidentésTiers[accidentésTiers$CHOIXACC %in% c(71:75), "ACC"] <- 15
+    
+    # str(accidentésTiers$ACC)
+    
+    contingenceAcc <- table(accidentésTiers$ACC, accidentésTiers$AUTREUSAGER)
+    plot(contingenceAcc)
+    df_contingence <- as.data.frame.matrix(contingenceAcc)
+    colnames(df_contingence) <- c("Non","Oui")
+    
+    plot_graph <- data.frame(type = c(1:15), nb = df_contingence$Oui, Présence_Tiers = "Oui")
+    plot_graph2 <- data.frame(type = c(1:15), nb = df_contingence$Non, Présence_Tiers = "Non")
+    final_plot_graph <- rbind(plot_graph, plot_graph2) 
+    
+    ggplot(data = subset(final_plot_graph, ACC %in% input$typeAccident), aes(type, nb)) + 
+    geom_bar(stat= "identity", aes(fill = Présence_Tiers)) + 
+    scale_fill_manual(values=c("#004d7e", "#64be29")) + 
+    labs(title="Présence d'un tiers en fonction du type d'accident", x="Type d'accident", y="Nombre de réponse")
+  })
+  
+  output$representationJeunesPlot <- renderPlot({
+    
+    dateNaissance <- as.Date(data$DATE_NAISS, format="%d%b%Y")
+    anneesNaissance <- as.numeric(format(dateNaissance, "%Y"))
+    age <- 2018 - anneesNaissance
+    
+    accidentEtAge <- select(subset(data, data$ACCIDENT == 1), "NBACC1", "NBACC2","NBACC3","NBACC4","NBACC5","NBACC6","NBACC7","NBACC8", "NBACC9","NBACC10","NBACC11","NBACC12","NBACC13","NBACC14","DATE_NAISS")
+    accidentEtAge$age <- 2018 - as.numeric(format(as.Date(accidentEtAge$DATE_NAISS, format="%d%b%Y"), format="%Y")) 
+    
+    accidentPropre <- subset(accidentEtAge, accidentEtAge$age < 100)
+    accidentPropre$tranche <- accidentPropre$age
+    levels(accidentPropre$tranche) <- c(levels(accidentPropre$tranche), "vingtaine", "trentaine", "quarantaine", "cinquantaine", "soixantaine", "70aine", "80aine", "90aine")
+    accidentPropre[accidentPropre$tranche %in% c(20:29), "tranche"] <- "vingtaine"
+    accidentPropre[accidentPropre$tranche %in% c(30:39), "tranche"] <- "trentaine"
+    accidentPropre[accidentPropre$tranche %in% c(40:49), "tranche"] <- "quarantaine"
+    accidentPropre[accidentPropre$tranche %in% c(50:59), "tranche"] <- "cinquantaine"
+    accidentPropre[accidentPropre$tranche %in% c(60:69), "tranche"] <- "70aine"
+    accidentPropre[accidentPropre$tranche %in% c(70:79), "tranche"] <- "80aine"
+    accidentPropre[accidentPropre$tranche %in% c(80:89), "tranche"] <- "90aine"
+    
+    
+    accidentPropre$DATE_NAISS <- NULL 
+    
+    maFonction <- function(final, testEncore){
+      test <- 0 
+      for(i in 1:7){
+        total <- 0 
+        for(j in 1:ncol(testEncore)){ 
+          total <- total + as.numeric(colnames(testEncore)[j]) * testEncore[i,j]
+        }
+        test[i] <- total
+      } 
+      test <- as.table(test)
+      rownames(test) <- rownames(testEncore)
+      final <- rbind(final,test)
+      rownames(final) <- NULL
+      return(final)
+    }
+    
+    final <- 0
+    final <- as.table(final)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC1)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC2)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC3)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC4)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC5)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC6)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC7)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC8)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC9)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC10)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC11)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC12)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC13)
+    final <- maFonction(final, totalAccidentX)
+    totalAccidentX <- table(accidentPropre$tranche, accidentPropre$NBACC14)
+    final <- maFonction(final, totalAccidentX)
+    final <- final[-1,]
+    q
+    finalProportion <- final 
+    for(i in 1:nrow(final)){
+      finalProportion[i,] <- prop.table(final[i,])
+    }
+    
+    finalProportionAllType <- final[1,]
+    for(i in 1:ncol(final)){
+      finalProportionAllType[i] <- sum(final[,i])
+    }
+    finalProportionAllType <- prop.table(finalProportionAllType)
+    
+    dat <- data.frame(
+      type = c(1:14),
+      proportion = finalProportion[,"vingtaine"]
+    )
+    
+    ggplot(data= subset(dat, type %in% input$typeAccident), aes(x=type, y=proportion)) +
+      geom_bar(stat="identity", fill = "#004d7e") + 
+      geom_hline(aes(yintercept = finalProportionAllType["vingtaine"]), color="#64be29", linetype="dashed") + 
+      theme_light()
+    
+    
+  })
+  
 })
